da601c51df2e48e2697e87305d2beeaeedcf43be
Ian Leggett <contactleggett@gmail.com>
2019-05-01 21:34:37 +0100

    Updated app with new event handlers

--- app.js
+++ app.js
@@ -1,10 +1,69 @@
-const getNotes = require('./notes.js')
 const chalk = require('chalk')
+const yargs = require('yargs')
+const note = require('./notes.js')
 
-const command = process.argv[2]
+// Create add command
 
-if (command === 'add') {
-  console.log('adding')
-} else if (command === 'remove') {
-  console.log('removing')
-}
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string',
+    }
+  },
+  handler: function (argv) {
+    note.addNote(argv.title, argv.body)
+  }
+})
+
+// Create remove command
+
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: false,
+      type: 'string',
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: false,
+      type: 'string',
+    }
+  },
+  handler: function (argv) {
+    note.removeNote(argv.title, argv.body)
+  }
+})
+
+// List command
+
+yargs.command({
+  command: 'list',
+  describe: 'List your notes',
+  handler: function () {
+    console.log('Listing all your notes')
+  }
+})
+
+// Read command
+
+yargs.command({
+  command: 'read',
+  describe: 'Reads your notes',
+  handler: function () {
+    console.log('Reading your notes')
+  }
+})
+
+yargs.parse()
